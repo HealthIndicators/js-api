59e0021221f3361e01e841fbcdc44fbf5e2ad3b9
Josh McCullough <jmccullough@s-3.com>
2015-05-27 12:44:36 -0400

    Added In/NotIn functionality to Criterion.

--- Content/Scripts/API/hiw/Criterion.js
+++ Content/Scripts/API/hiw/Criterion.js
@@ -6,19 +6,34 @@ var hiw;
         function Criterion(field, operator, value) {
             if (operator === void 0) { operator = 2 /* Equal */; }
             if (value === void 0) { value = null; }
+            if (field == null)
+                throw new Error("Parameter 'field' is required.");
             this.name = (field instanceof hiw.PropertyMap ? field.apiName : field.toString());
             this.operator = operator;
             this.value = value;
         }
         /** Converts this instance to JSON in the format the HIW API expects. */
         Criterion.prototype.toJSON = function () {
-            var json = {
-                "__type": "SearchParameter:#S3.Common.Search",
-                Name: this.name,
-                Operator: this.operator
-            };
-            if (this.operator != 6 /* Null */ && this.operator != 7 /* NotNull */)
-                json.Value = this.value;
+            var json = null;
+            if (this.operator == 8 /* In */ || this.operator == 9 /* NotIn */) {
+                if (this.value != null && !Array.isArray(this.value))
+                    throw new Error("Value provided for \"" + this.operator + "\" criterion must be an array.");
+                else if (this.value != null && this.value.length > 0) {
+                    var group = new hiw.Group(this.operator == 8 /* In */ ? 1 /* Or */ : 0 /* And */);
+                    for (var i = 0; i < this.value.length; i++)
+                        group.add(this.name, (this.operator == 8 /* In */ ? 2 /* Equal */ : 3 /* NotEqual */), this.value[i]);
+                    json = group.toJSON();
+                }
+            }
+            else {
+                json = {
+                    "__type": "SearchParameter:#S3.Common.Search",
+                    Name: this.name,
+                    Operator: this.operator
+                };
+                if (this.operator != 6 /* Null */ && this.operator != 7 /* NotNull */)
+                    json.Value = this.value;
+            }
             return json;
         };
         return Criterion;
--- Content/Scripts/API/hiw/Criterion.ts
+++ Content/Scripts/API/hiw/Criterion.ts
@@ -12,6 +12,9 @@
 
         /** Creates a new Criterion instance with the specified name, operator and value. */
         constructor(field: PropertyMap|string, operator: Operator = Operator.Equal, value: any = null) {
+            if (field == null)
+                throw new Error("Parameter 'field' is required.");
+
             this.name = (<any>field instanceof PropertyMap ? (<PropertyMap>field).apiName : field.toString());
             this.operator = operator;
             this.value = value;
@@ -19,14 +22,30 @@
             
         /** Converts this instance to JSON in the format the HIW API expects. */
         public toJSON(): Object {
-            var json: any = {
-                "__type": "SearchParameter:#S3.Common.Search",
-                Name: this.name,
-                Operator: this.operator
-            };
+            var json: any = null;
 
-            if (this.operator != Operator.Null && this.operator != Operator.NotNull)
-                json.Value = this.value;
+            if (this.operator == Operator.In || this.operator == Operator.NotIn) {
+                if (this.value != null && !Array.isArray(this.value))
+                    throw new Error("Value provided for \"" + this.operator + "\" criterion must be an array.");
+                else if (this.value != null && this.value.length > 0) {
+                    var group = new Group(this.operator == Operator.In ? FilterType.Or : FilterType.And);
+
+                    for (var i = 0; i < this.value.length; i++)
+                        group.add(this.name,(this.operator == Operator.In ? Operator.Equal : Operator.NotEqual), this.value[i]);
+
+                    json = group.toJSON();
+                }
+            }
+            else {
+                json = {
+                    "__type": "SearchParameter:#S3.Common.Search",
+                    Name: this.name,
+                    Operator: this.operator
+                };
+
+                if (this.operator != Operator.Null && this.operator != Operator.NotNull)
+                    json.Value = this.value;
+            }
 
             return json;
         }
