036f45f2e9986659263b39deb0db73a94df8900c
Josh McCullough <jmccullough@s-3.com>
2015-06-12 13:25:59 -0400

    Added Build All script.

---
+++ Content/Scripts/API/Build All.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Microsoft SDKs\TypeScript\1.4\tsc" @Lite.txt
+"C:\Program Files (x86)\Microsoft SDKs\TypeScript\1.4\tsc" @Full.txt
+"C:\Program Files (x86)\Microsoft SDKs\TypeScript\1.4\tsc" @Test.txt
